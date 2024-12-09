cf1300571f8257846c0aaae1ba2510873afecf50
OsielMarquez <L21550345@chihuahua2.tecnm.mx>
2024-12-08 19:38:48 -0600

    cambios de lugar

---
+++ U3/js/p1.js
@@ -0,0 +1,7 @@
+const nombre = "Osiel Isaac Marquez Aguilar";
+const numControl = 21550345;
+
+console.log("Nombre: ", nombre);
+alert("Osiel Isaac Marquez Aguilar");
+console.log("Número de control:", numControl);
+alert("21550345");
